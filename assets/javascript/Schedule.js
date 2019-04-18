1b6549bba8f552741dce1edd6eee3567b2c5bac4
Adventure 2019 <caliew26@gmail.com>
2019-04-18 18:52:13 -0400

    pseudocode

---
+++ assets/javascript/Schedule.js
@@ -0,0 +1,50 @@
+// add train minimum requirements 
+    //adding a train to the schedule - this is a div by itself
+        // Train name
+            //max number of characters is 50 (includes spaces but cannot be just spaces)
+            //input field
+
+        // destination
+            //max number of characters is 25
+            //input field
+
+        //?  First train time -- military time HH:mm (this is when the first train will depart)
+            //input field
+
+        // frequency -- in minutes (this is how often the train departs)
+            //input field
+
+        //next arrival -- will be total minutes
+            //input field
+            //update the arrival time in realtime (screen refreshes every 1 minute, if frequency is 30 minutes, next arrival is the actual arrival time (say at 545pm) that is how many minutes from "now" - that is the countdown)
+
+        //submit button -- 
+            //will need to be disabled on initial page load
+            //once all field are populated then enable
+                //all fields are required prior to submit
+
+
+    //display div
+        //columns
+            // train name
+            // destination
+            // frequency
+            // next arrival
+            // minutes away
+
+        //make all columns sortable
+
+// calculate when the train will arrive 
+    //this will be current time plus the frequency
+
+    
+//disney railway style theme
+
+
+
+//add update / remove buttons with functionality behind them
+
+// user login with google username/password or github username/password
+
+//search for train, highlight that train or remove all and display only that 1 search request
+
